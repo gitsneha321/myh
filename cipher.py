483c0f232f33a7eeea977408e97a82144db2ff16
gitsneha321 <aaasneha88@gmail.com>
2024-09-09 23:51:33 -0700

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,16 @@
+def encrypt(text,s):
+    result=""
+    for i in range(len(text)):
+        char=text[i]
+        if(char.isupper()):
+            result+=chr((ord(char)+s-65)%26+65)
+        else:
+            result+=chr((ord(char)+s-65)%26+97)
+    return result
+text="attackationce"
+s=4
+print("text:"+text)
+print("shift:"+str(s))
+print("cipher:"+encrypt(text,s))
+            
+            
